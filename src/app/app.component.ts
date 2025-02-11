3345bcdedbbc7df12c697c201e1fdddecb63fc04
João Pedro Meister Ivacow <joaopedromeisterivacow@gmail.com>
2025-02-10 22:19:50 -0300

    fetch: Criação do cabeçalho e Rodapé

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './componentes/header/header.component';
+import { RodapeComponent } from "./componentes/rodape/rodape.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, RodapeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
